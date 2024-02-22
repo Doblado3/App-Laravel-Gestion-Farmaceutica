45901ff4bdce50717dc1cdb632b8de4ca2b12d22
Ronar98 <naranjopozasrodrigo@gmail.com>
2024-02-22 19:34:45 +0100

    651

--- app/Models/Paciente.php
+++ app/Models/Paciente.php
@@ -13,4 +13,9 @@ class Paciente extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
