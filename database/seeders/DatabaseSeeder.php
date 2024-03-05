6f243faceab87b47bc279766bc2ec66cb78430d5
Doblado3 <Pablodoza11@gmail.com>
2024-03-05 12:26:45 +0100

    .

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            UserSeeder::class, FarmaceuticoSeeder::class, PacienteSeeder::class
+            UserSeeder::class, FarmaciaSeeder::class, FarmaceuticoSeeder::class
         ]);
     }
 }
--- database/seeders/FarmaceuticoSeeder.php
+++ database/seeders/FarmaceuticoSeeder.php
@@ -18,11 +18,15 @@ class FarmaceuticoSeeder extends Seeder
             [
                 'dni' => "12345678A",
                 'fecha_contratacion' => "2021-01-01",
+                'user_id' => 2,
+                
             
             ],
             [
                 'dni' => "87654321B",
                 'fecha_contratacion' => "2022-06-01",
+                'user_id' => 2,
+                
                 
             ],
         ]);
