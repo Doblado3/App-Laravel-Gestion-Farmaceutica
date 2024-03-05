efc702368b6eef909baf67f819694f6803b6d485
Doblado3 <Pablodoza11@gmail.com>
2024-03-05 18:04:45 +0100

    .

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -43,7 +43,7 @@
     <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
         <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
             <div style="padding:1em 25em;">
-                <img  style="width:100%;" src="public/images">
+                
             </div>
             <div class="grid grid-cols-1 md:grid-cols-1">
                 <div class="p-6">
