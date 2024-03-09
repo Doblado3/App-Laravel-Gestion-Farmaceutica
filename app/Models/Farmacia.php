c6de7c48705ce93b10d3e6ea6fc0610fe3116763
Doblado3 <Pablodoza11@gmail.com>
2024-03-09 16:58:22 +0100

    Vuelta atrás

--- app/Models/Farmacia.php
+++ app/Models/Farmacia.php
@@ -26,4 +26,5 @@ class Farmacia extends Model
     {
         return $this->hasMany(MedicamentoProveedor::class);
     }
+    
 }
--- routes/web.php
+++ routes/web.php
@@ -18,18 +18,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-
-
 Route::resources([
     'farmaceuticos' => FarmaceuticoController::class,
     'farmacias' => FarmaciaController::class,
@@ -37,4 +25,3 @@ Route::resources([
 //Route::get('/farmaceuticos', [FarmaceuticoController::class, 'index'])->name('farmaceuticos.index');
 
 
-require __DIR__.'/auth.php';
