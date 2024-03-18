024078128e8ea514e8d40e3ccca59a04f7f0f337
Doblado3 <Pablodoza11@gmail.com>
2024-03-18 21:49:57 +0100

    Desarrollando el welcome final

--- resources/views/welcome2.blade.php
+++ resources/views/welcome2.blade.php
@@ -6,7 +6,7 @@
 
 
     <title>ParaFarmacial</title>
-    <link rel="icon" type="image/svg" href="/components/icono-farmacia1.blade.php">
+    <link rel="icon" href="/components/icono-farmacia1.blade.php">
     <meta name="description" content="Digitaliza de forma completa la gestión de tu local farmaceutico. Controla
     tu ventas y tus compras, puediendo comparar entre distintos proveedores, y lleva un listado al completo de tus pacientes
     y de tus trabajadores">
@@ -16,37 +16,59 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 </head>
 <body class="font-[Poppins] bg-gradient-to-t from-[#c2f4fb] to-[#c2ddfb] h-screen">
     <header class="bg-white">
-        <nav class="">
+        <nav class="flex justify-between items-center w-[92%]">
             <div>
                 <img class="w-16" src="https://cdn-icons-png.flaticon.com/512/5968/5968204.png" alt="...">
             </div>
-            <div class="">
-                <ul>
+            <div class="nav-links duration-500 md:static absolute bg-white md:min-h-fit min-h-[60vh] left-0 top-[-100%] md:w-auto w-full flex items-center px-5">
+                <ul class="flex md:flex-row flex-col md:items-center md:gap-[4vw] gap-8">
                     <li>
-                        <a href="#">Products</a>
+                        <a class="hover:text-gray-300" href="#">Productos</a>
                     </li>
                     <li>
-                        <a href="#">Solution</a>
+                        <a class="hover:text-gray-300" href="#">Redes</a>
                     </li>
                     <li>
-                        <a href="#">Resource</a>
+                        <a class="hover:text-gray-300" href="#">Acceder</a>
                     </li>
                     <li>
-                        <a href="#">Developers</a>
+                        <a class="hover:text-gray-300" href="#">Registrarse</a>
                     </li>
                     <li>
-                        <a href="#">Pricing</a>
+                        <a class="hover:text-gray-300" href="#">Registrarse como farmaceutico</a>
                     </li>
                 </ul>
 
             </div> 
-            <div>
-                <button class="bg-[#c2ddfb] text-white px-5 py-2">Sign in</button>
+            <div class="flex items-center gap-6">
+                <button class="bg-[#c2ddfb] text-white px-5 py-2 rounded-full hover:bg-[#87ac]">Acceder</button>
+                <ion-icon onclick="onToggleMenu(this)" name="menu-outline" class="text-3xl cursor-pointer md:hidden"></ion-icon>
             </div>
         </nav>
     </header>
+    <section class="mt-8">
+        <div>
+            Aquí iran los objetivos
+        </div>
+    </section>
+    <footer>
+        Aquí ira un poco el spam y la "publicidad"
+    </footer>
+
+    
+    
+    
+    <script>
+        const navLinks = document.querySelector('.nav-links')
+        function onToggleMenu(e){
+            e.name = e.name === 'menu-outline' ? 'close' : 'menu-outline'
+            navLinks.classList.toggle('top-[8%]')
+        }
+    </script>
 </body>
 </html>
