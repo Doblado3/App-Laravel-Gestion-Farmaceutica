331fe167b29836d40f2c5343ae547594cfffd484
Doblado3 <Pablodoza11@gmail.com>
2024-03-19 11:13:47 +0100

    Añadimos las ventas para comenzar con la lógica de los pacientes

--- database/factories/VentaFactory.php
+++ database/factories/VentaFactory.php
@@ -5,9 +5,9 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Inventario>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Venta>
  */
-class InventarioFactory extends Factory
+class VentaFactory extends Factory
 {
     /**
      * Define the model's default state.
