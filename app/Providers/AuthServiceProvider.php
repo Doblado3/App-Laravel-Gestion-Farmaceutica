a20fced242644fc4897ac9538898bcbdb2ca8d99
Doblado3 <Pablodoza11@gmail.com>
2024-04-09 09:51:37 +0200

    .

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -14,6 +14,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         Farmaceutico::class => FarmaceuticoPolicy::class,
+        Venta::class => VentaPolicy::class,
     ];
 
     /**
