2d93e290016d48b16748ccc5de5697e78aeaa42e
Ronar98 <naranjopozasrodrigo@gmail.com>
2024-04-30 11:23:18 +0200

    30042024123

--- app/Rules/DNI.php
+++ app/Rules/DNI.php
@@ -5,17 +5,19 @@ namespace App\Rules;
 use Closure;
 use Illuminate\Contracts\Validation\ValidationRule;
 
-class DNI implements esDNIValido($dni){
-    if(strlen($dni)!=9 || !is_numeric(substr($dni, 0, 8)) || !ctype_alpha(substr($dni, -1))){
-        return false;
-    }
-    $letrasDNI = "TRWAGMYFPDXBNJZSQVHLCKE";
-    $indice = substr($dni, 0, 8) % 23;
-    $letraCalculada = $letrasDNI[$indice]; #letra dni correspondiente
-    $letraDNI = strtoupper(substr($dni, -1));
-    if ($letraCalculada != $letraDNI){
-        return false;
-    }
+class DNI implements ValidationRule
+{
+    private function esDNIValido($dni){
+        if(strlen($dni)!=9 || !is_numeric(substr($dni, 0, 8)) || !ctype_alpha(substr($dni, -1))){
+            return false;
+        }
+        $letrasDNI = "TRWAGMYFPDXBNJZSQVHLCKE";
+        $indice = substr($dni, 0, 8) % 23;
+        $letraCalculada = $letrasDNI[$indice]; #letra dni correspondiente
+        $letraDNI = strtoupper(substr($dni, -1));
+        if ($letraCalculada != $letraDNI){
+            return false;
+        }
     /**
      * Run the validation rule.
      *
