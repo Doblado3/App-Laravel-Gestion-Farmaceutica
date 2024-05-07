c02e9f5736092072c486e8ef5a83db027276b4ac
Ronar98 <naranjopozasrodrigo@gmail.com>
2024-05-07 12:05:37 +0200

    07052024

--- app/Rules/DNI.php
+++ app/Rules/DNI.php
@@ -3,16 +3,25 @@
 namespace App\Rules;
 
 use Closure;
-use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Contracts\Validation\Rule;
 
-class DNI implements ValidationRule
+class DNI implements Rule
 {
-    private function esDNIValido($dni){
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+
+    public function passes($attribute, $value)
+    {
         #$letrasDNI = "TRWAGMYFPDXBNJZSQVHLCKE";
         #$indice = (int)substr($dni, 0, 8) % 23;
         #$letraCalculada = $letrasDNI[$indice]; #letra dni correspondiente
         #$letraDNI = strtoupper(substr($dni, -1));
-        if(strlen($dni)!=9 || !is_numeric(substr($dni, 0, 8)) || !ctype_alpha(substr($dni, -1))){
+        if(strlen($value)!=9 || !is_numeric(substr($value, 0, 8)) || !ctype_alpha(substr($value, -1))){
             return false;
         #} elseif ($letraCalculada != $letraDNI){
         #    return false;
@@ -20,16 +29,15 @@ class DNI implements ValidationRule
             return true;
         }
     }
-    /**
-     * Run the validation rule.
-     *
-     * @param  \Closure(string): \Illuminate\Translation\PotentiallyTranslatedString  $fail
-     */
 
-    public function validate(string $attribute, mixed $value, Closure $fail): void
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
     {
-        if (!$this->esDNIValido($value)) {
-            $fail(__('validation.custom.dni')[$attribute]);
-        }
+        return 'El DNI introducido no es valido';
     }
+
 }
