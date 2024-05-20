3eb9284dff19233d7a5bb756c3405eb4d72cb916
Doblado3 <Pablodoza11@gmail.com>
2024-05-20 17:01:27 +0200

    Postman Api Rest

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FarmaceuticoController;
+use App\Http\Controllers\FarmaciaController;
+use App\Http\Controllers\VentaController;
+use App\Http\Controllers\PacienteController;
+use App\Http\Controllers\MedicamentoController;
+use App\Http\Controllers\ProfileController;
+use App\Models\Venta;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,9 +26,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-//API - TODO
-/*
-Route::get('farmaceuticos', function(){
-    return Farmaceutico::all();
+
+
+Route::get('/farmaceuticos', [FarmaceuticoController::class,'index']);
+Route::post('/farmaceuticos', [FarmaceuticoController::class,'store']);
+Route::get('/ventas', function(){
+    return Venta::all();
 });
-*/
